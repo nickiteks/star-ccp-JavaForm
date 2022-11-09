23e3e951fb8b009ca5bab25dff4b005177002c12
nickiteks <splint38@gmail.com>
2022-11-10 02:29:41 +0400

    add fileChooser

--- src/Assistant/main.java
+++ src/Assistant/main.java
@@ -5,27 +5,40 @@ import javax.tools.JavaFileManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class main {
     public static void main(String[] args){
 
         JFrame frame = new JFrame();
-        JLabel lbGeometr = new JLabel("Файл геометрии:");
-        JButton btnGeometry = new JButton("Выбрать геометрию");
+        JLabel lbGeometry = new JLabel("Geometry File");
+        JButton btnGeometry = new JButton("Choose Geometry");
         btnGeometry.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //System.out.println("aaaa");
+                JFileChooser fileChooser = new JFileChooser();
+                int response = fileChooser.showOpenDialog(null);
+
+                if(response == JFileChooser.APPROVE_OPTION){
+                    File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                    lbGeometry.setText(file.toString());
+                }
             }
         });
 
+        JLabel lbExcel = new JLabel("ExcelFile");
+        JButton btnExcel = new JButton("Choose Excel File");
+
         JPanel jpForm = new JPanel();
         jpForm.setLayout(new GridLayout(2,2));
-        jpForm.add(lbGeometr);
+        jpForm.add(lbGeometry);
         jpForm.add(btnGeometry);
+        jpForm.add(lbExcel);
+        jpForm.add(btnExcel);
 
         JPanel jpMainPannel = new JPanel();
         jpMainPannel.setLayout(new BorderLayout(10,10));
+        jpMainPannel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         jpMainPannel.add(jpForm,BorderLayout.NORTH);
 
         frame.add(jpMainPannel);
--- src/Assistant/test.java
+++ src/Assistant/test.java
@@ -10,10 +10,10 @@ import java.awt.event.ActionListener;
 public class test extends StarMacro {
 
     public void execute() {
-         
+
         JFrame frame = new JFrame();
-        JLabel lbGeometr = new JLabel("Файл геометрии:");
-        JButton btnGeometry = new JButton("Выбрать геометрию");
+        JLabel lbGeometry = new JLabel("Geometry File");
+        JButton btnGeometry = new JButton("Choose Geometry");
         btnGeometry.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -21,13 +21,19 @@ public class test extends StarMacro {
             }
         });
 
+        JLabel lbExcel = new JLabel("ExcelFile");
+        JButton btnExcel = new JButton("Choose Excel File");
+
         JPanel jpForm = new JPanel();
         jpForm.setLayout(new GridLayout(2,2));
-        jpForm.add(lbGeometr);
+        jpForm.add(lbGeometry);
         jpForm.add(btnGeometry);
+        jpForm.add(lbExcel);
+        jpForm.add(btnExcel);
 
         JPanel jpMainPannel = new JPanel();
         jpMainPannel.setLayout(new BorderLayout(10,10));
+        jpMainPannel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         jpMainPannel.add(jpForm,BorderLayout.NORTH);
 
         frame.add(jpMainPannel);
